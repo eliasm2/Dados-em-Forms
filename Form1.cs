8153e33fc2e4a5014bd20db481e98250ebb02ea3
Elias Linhares M. Junior <44944016+eliasm2@users.noreply.github.com>
2020-04-10 22:40:45 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dados_em_Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 x = new Form2(this);        // Instância - Passa o Form de Contagem como Parâmetro
+            x.Show();                       // Mostra o Form de Incremento
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dados_em_Forms
+{
+    public partial class Form2 : Form
+    {
+        Form1 FormContagem;
+
+        public Form2(Form1 F)
+        {
+            InitializeComponent();
+            
+            FormContagem = F;
+
+            label2.Text = FormContagem.textBox1.Text;                // Inicializa o Label
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Valor;
+
+            Valor = int.Parse(FormContagem.textBox1.Text);
+            
+            Valor++;
+
+            FormContagem.textBox1.Text = Valor.ToString();                     // TextBox1 precisa ser public (Design)
+
+            label2.Text = Valor.ToString(); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int Valor;
+
+            Valor = int.Parse(FormContagem.textBox1.Text);
+
+            Valor--;
+
+            FormContagem.textBox1.Text = Valor.ToString();                     // TextBox1 precisa ser public (Design)
+
+            label2.Text = Valor.ToString(); 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
